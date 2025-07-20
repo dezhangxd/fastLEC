3885a852974e12228410113f09a128259c6965e9
Xindi zhang <dezhangxd@163.com>
2025-07-20 11:57:26 +0800

    upd param

--- src/simu.cpp
+++ src/simu.cpp
@@ -319,7 +319,7 @@ int fastLEC::ISimulator::run_ies_round(uint64_t r)
     }
 
     // check result
-    int res = 0;    
+    int res = 0;
     if (local_mems[glob_es.PO_lit] != 0u)
         res = 10;
     else
@@ -653,13 +653,13 @@ fastLEC::ret_vals fastLEC::Simulator::run_pbits_pes(unsigned n_t)
             res = 20;
     }
 
-    printf("c [iES] result = %d [bv:para:batch=%d:%d:%d] [bv_w = %3d] [nGates = %5lu] [nPI = %3lu] [num_bv = %u] [time = %.2f]\n",
-        res,
-        this->bv_bits, this->para_bits, this->batch_bits,
-        Param::get().custom_params.es_bv_bits,
-        xag.used_gates.size(), xag.PI.size(),
-        is->glob_es.mem_sz,
-        ResMgr::get().get_runtime() - start_time);
+    printf("c [piES] result = %d [bv:para:batch=%d:%d:%d] [bv_w = %3d] [nGates = %5lu] [nPI = %3lu] [num_bv = %u] [time = %.2f]\n",
+           res,
+           this->bv_bits, this->para_bits, this->batch_bits,
+           Param::get().custom_params.es_bv_bits,
+           xag.used_gates.size(), xag.PI.size(),
+           is->glob_es.mem_sz,
+           ResMgr::get().get_runtime() - start_time);
 
     if (res == 0)
         return ret_vals::ret_UNK;
@@ -671,7 +671,107 @@ fastLEC::ret_vals fastLEC::Simulator::run_pbits_pes(unsigned n_t)
 
 fastLEC::ret_vals fastLEC::Simulator::run_round_pes(unsigned n_t)
 {
-    return ret_vals::ret_UNK;
+    double start_time = ResMgr::get().get_runtime();
+    assert(is == nullptr);
+    is = std::make_unique<fastLEC::ISimulator>();
+    is->init_glob_ES(xag);
+    cal_es_bits(1);
+
+    std::vector<std::thread> threads;
+    std::atomic<bool> found_sat(false);
+    std::atomic<bool> cutted(false);
+
+    auto worker = [&](unsigned long long start_round, unsigned long long end_round)
+    {
+        std::vector<BitVector> loc_mem(is->glob_es.mem_sz, BitVector(1 << this->bv_bits));
+
+        for (unsigned long long round = start_round; round < end_round; round++)
+        {
+            if (Param::get().verbose > 1 && round % 100000 == 0)
+                printf("c [iES] %6.2f%% : round %lld / %llu \n", (double)round / (end_round - start_round) * 100, round, end_round - start_round);
+            fflush(stdout);
+            loc_mem[0].reset();
+            loc_mem[1].set();
+
+            unsigned long long i = 0;
+            unsigned bvb = std::min(is->glob_es.PI_num, this->bv_bits);
+            for (i = 0; i < bvb; i++)
+                loc_mem[i + 2].u64_pi(i);
+
+            for (i = bvb; i < is->glob_es.PI_num; i++)
+            {
+                int k = (round >> (i - bvb)) & 1ull;
+                if (k == 1)
+                    loc_mem[i + 2].set();
+                else
+                    loc_mem[i + 2].reset();
+            }
+
+            for (i = 0; i < is->glob_es.n_ops; i++)
+            {
+                operation *op = &is->glob_es.ops[i];
+
+                if (op->type == OP_AND)
+                    loc_mem[op->addr1] = loc_mem[op->addr2] & loc_mem[op->addr3];
+                else if (op->type == OP_XOR)
+                    loc_mem[op->addr1] = loc_mem[op->addr2] ^ loc_mem[op->addr3];
+                else if (op->type == OP_NOT)
+                    loc_mem[op->addr1] = ~loc_mem[op->addr2];
+            }
+
+            if (loc_mem[is->glob_es.PO_lit].has_one())
+            {
+                found_sat.store(true);
+                break;
+            }
+        }
+    };
+
+    try
+    {
+        fflush(stdout);
+        unsigned long long round_num = 1llu << batch_bits;  
+        for (uint64_t i = 0; i < n_t; i++){
+            unsigned long long start_round = i * round_num / n_t;
+            unsigned long long end_round;
+            if(i == n_t - 1)
+                end_round = round_num;
+            else
+                end_round = (i + 1) * round_num / n_t;
+            threads.emplace_back(worker, start_round, end_round);
+        }
+
+        for (auto &t : threads)
+            t.join();
+    }
+    catch (const std::exception &e)
+    {
+        printf("c [pEPS] exception: %s\n", e.what());
+    }
+
+    int res = 0;
+    if (!cutted.load())
+    {
+        if (found_sat.load())
+            res = 10;
+        else
+            res = 20;
+    }
+
+    printf("c [piES] result = %d [bv:round:batch=%d:%d] [bv_w = %3d] [nGates = %5lu] [nPI = %3lu] [num_bv = %u] [time = %.2f]\n",
+           res,
+           this->bv_bits, this->batch_bits,
+           Param::get().custom_params.es_bv_bits,
+           xag.used_gates.size(), xag.PI.size(),
+           is->glob_es.mem_sz,
+           ResMgr::get().get_runtime() - start_time);
+
+    if (res == 0)
+        return ret_vals::ret_UNK;
+    else if (res == 10)
+        return ret_vals::ret_SAT;
+    else
+        return ret_vals::ret_UNS;
 }
 
 fastLEC::ret_vals fastLEC::Prove_Task::para_es(int n_thread)
@@ -681,11 +781,11 @@ fastLEC::ret_vals fastLEC::Prove_Task::para_es(int n_thread)
     if (Param::get().custom_params.use_pes_pbit)
     {
         int n_t = simu.cal_pes_threads(n_thread);
-        simu.run_pbits_pes(n_t);
+        return simu.run_pbits_pes(n_t);
     }
     else
     {
-        simu.run_round_pes(n_thread);
+        return simu.run_round_pes(n_thread);
     }
 
     return ret_vals::ret_UNK;
