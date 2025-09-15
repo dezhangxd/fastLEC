bf73014c91bcb7514b09bf76ab5f0d5776e8b0c4
xindi <dezhangxd@163.com>
2025-09-15 11:09:14 +0800

    upd log

--- test.sh
+++ test.sh
@@ -9,14 +9,11 @@ BUILD_DIR="build"
 # ulimit -t 5
 
 command="-m ES -c 1 -v 2 -t 5 -p ies_u64 1"
-command="-m pES -c 8 -v 2 -t 5 -p use_pes_pbit 1"
+command="-m pES -c 4 -v 2 -t 20 -p use_pes_pbit 0"
 
-./${BUILD_DIR}/bin/fastLEC -i ../ins/miter_right/bit2/mul2o2.aig $command |gnomon
-# ./${BUILD_DIR}/bin/fastLEC -i ../ins/miter_right/bit5/mul5o5.aig $command |gnomon
+# ./${BUILD_DIR}/bin/fastLEC -i ../ins/miter_right/bit2/mul2o2.aig $command |gnomon
 ./${BUILD_DIR}/bin/fastLEC -i ../ins/miter_right/bit13/mul13o13.aig $command |gnomon
-./${BUILD_DIR}/bin/fastLEC -i ../ins/miter_right/bit16/mul16o16.aig $command |gnomon
+# ./${BUILD_DIR}/bin/fastLEC -i ../ins/miter_right/bit16/mul16o16.aig $command |gnomon
 
-
-# ./${BUILD_DIR}/bin/fastLEC -i ../ins/miter_error/bit2/miter2o2.aig $command |gnomon
-./${BUILD_DIR}/bin/fastLEC -i ../ins/miter_error/bit13/miter13o13.aig $command |gnomon
+# ./${BUILD_DIR}/bin/fastLEC -i ../ins/miter_error/bit13/miter13o13.aig $command |gnomon
 
